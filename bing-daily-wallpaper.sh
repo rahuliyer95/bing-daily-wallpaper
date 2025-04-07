76f8cd59e1f8f33f27cd18ef42fbbe7132ab5cc8
Rahul Iyer <rahuliyer573@gmail.com>
2025-04-07 13:48:05 -0700

    feat: use `shortcuts` to change wallpaper on macOS

--- bing-daily-wallpaper.sh
+++ bing-daily-wallpaper.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-OS="$(uname)"
-# REGION_IN="en-IN"
-REGION_US="en-US"
-BING_API="https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$REGION_US"
+set -eux -o pipefail
+
+exec > >(sed -u "s/^/[$(date '+%Y-%m-%d %H:%M:%S')] /")
+
+exec 2>&1
 
 check_internet() {
-  ping -c 1 8.8.8.8 > /dev/null 2>&1
+  ping -c 1 8.8.8.8 >/dev/null 2>&1
 }
 
 while ! check_internet; do
@@ -14,14 +15,16 @@ while ! check_internet; do
   sleep 5
 done
 
-WALLPAPER_URL="https://bing.com$(curl -sSL "$BING_API" | jq -r '.images[0].url')"
-
+OS="$(uname)"
 case "$OS" in
-  *Darwin*)
-    echo "Downloading wallpaper"
-    curl -sSLo "$HOME/Pictures/wallpaper.jpg" "$WALLPAPER_URL"
-    echo "Setting wallpaper"
-    osascript -e "tell application \"Finder\" to set desktop picture to \"$HOME/Pictures/wallpaper.jpg\" as POSIX file"
-    killall Dock
-    ;;
+*Darwin*)
+  # rm -f "$HOME/Pictures/Shortcuts Desktop Pictures/"*.jpeg
+  shortcuts run "Run Bing Daily Wallpaper"
+  ;;
+*)
+  # REGION_IN="en-IN"
+  # REGION_US="en-US"
+  # BING_API="https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$REGION_US"
+  # WALLPAPER_URL="https://bing.com$(curl -sSL "$BING_API" | jq -r '.images[0].urlbase')_UHD.jpg"
+  ;;
 esac
