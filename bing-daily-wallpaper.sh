f6d9ff82ba4905167ee21b1544b1198b67e95782
Rahul Iyer <rahuliyer573@gmail.com>
2025-05-13 20:44:16 -0700

    fix: ensure newly downloaded wallpaper is properly set

--- bing-daily-wallpaper.sh
+++ bing-daily-wallpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eux -o pipefail
+set -eu -o pipefail
 
 exec > >(sed -u "s/^/[$(date '+%Y-%m-%d %H:%M:%S')] /")
 
@@ -18,13 +18,14 @@ done
 OS="$(uname)"
 case "$OS" in
 *Darwin*)
-  # rm -f "$HOME/Pictures/Shortcuts Desktop Pictures/"*.jpeg
-  shortcuts run "Run Bing Daily Wallpaper"
+  rm -f "$HOME/Pictures/Shortcuts Desktop Pictures/"*.jpeg
+  shortcuts run "Bing Wallpaper for iOS" -i 0
+  killall Dock WallpaperAgent
   ;;
 *)
-  # REGION_IN="en-IN"
-  # REGION_US="en-US"
-  # BING_API="https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$REGION_US"
+  # REGION="en-IN"
+  # REGION="en-US"
+  # BING_API="https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$REGION"
   # WALLPAPER_URL="https://bing.com$(curl -sSL "$BING_API" | jq -r '.images[0].urlbase')_UHD.jpg"
   ;;
 esac
